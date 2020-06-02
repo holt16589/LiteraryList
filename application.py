70b552f83756accdd84b37e0e2083d4388e31cef
holt16589 <holt16589@gmail.com>
2020-06-01 21:46:18 -0400

    procfile fix

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-gunicorn.errors.HaltServeimport os, json
+import os, json
 from flask import Flask, session, render_template, redirect, url_for, request, jsonify, flash
 from flask_session import Session
 from sqlalchemy import create_engine
